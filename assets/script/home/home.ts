daf07fb01e379bbeab856233c251bafd9ebde84a
alex <343136121@qq.com>
2022-06-06 16:46:46 +0800

    登陆是否成功的标识

--- assets/script/home/home.ts
+++ assets/script/home/home.ts
@@ -94,9 +94,9 @@ export class home extends Component {
                     access_token:access_token,
                     host:host
                 },(succ,data)=>{
-                    console.log('succ and data',succ,data)
-                    succ = true;
-                    if(succ){
+                    console.log('data:',JSON.parse(data))
+                    let dataObj = JSON.parse(data)
+                    if(dataObj.success){
                         this.detectCaseParam();
                     }else{
                          // 如果不通过，则记录需要跳转的房间，再调用 Appnative.dkLogin();
